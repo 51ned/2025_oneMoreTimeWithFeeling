6d8af4e6129cf0eb43b191d981d2a8772fe77468
51ned <sustain@bk.ru>
2025-01-16 20:33:23 +0300

    ss-slider # 2

--- app/src/components/atoms/ss-slider.svelte
+++ app/src/components/atoms/ss-slider.svelte
@@ -7,32 +7,44 @@
     dir: 'rtl' | 'ltr'
   }
 
-  let { children, dir = 'rtl' }: ScrollsnapSlider = $props()
+  let { children, dir }: ScrollsnapSlider = $props()
 </script>
 
-<section class={cn('ss-slider', { 'ltr': dir === 'ltr' })}>
+
+<section class={cn('ss-slider', dir)}>
   {@render children()}
 </section>
+
 
 <style>
   .ss-slider {
     display: flex;
     gap: var(--indent-16-24);
     overflow-x: auto;
-    padding: var(--indent-16-24);
-    scroll-snap-type: x mandatory;
+    /* padding: 0 var(--indent-16-24); */
+    scroll-padding: var(--indent-16-24);
+    scroll-snap-type: x proximity;
+  } 
+
+  .ss-slider::-webkit-scrollbar {
+    display: none;
+  }
+
+  .ltr {
+    direction: ltr;
+  }
+
+  .rtl {
+    direction: rtl;
+    
   }
 
   :global(.ss-slider > *) {
     min-width: calc(100% - 36px);
-    scroll-snap-align: start;
+    scroll-snap-align: center;
   }
 
-  :global(.ltr, .ltr > *) {
-    transform: rotateY(180deg);
+  :global(.ss-slider.rtl > *) {
+    direction: ltr;
   }
-
-  /* .ss-slider::-webkit-scrollbar {
-    display: none;
-  } */
 </style>
